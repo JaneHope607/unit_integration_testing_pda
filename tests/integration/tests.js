802ee23539849d56e8cd6e13fe05c078a5f14f69
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-09-25 13:45:28 +0100

    chain multiple operations together test passes

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -28,8 +28,26 @@ describe('calculator functionality', function() {
     element(by.css('#operator_subtract')).click();
     element(by.css('#number4')).click();
     element(by.css('#operator_equals')).click();
-    running_total = element(by.css('#running_total'));
     expect(running_total.getAttribute('value')).to.eventually.equal('0')
+  })
+
+
+  it('arithmetical operations update the display with the result of the operation', function() {
+    element(by.css('#number4')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0')
+  })
+
+  it('should be able to chain multiple operations together', function() {
+    element(by.css('#number5')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('20')
   })
 
 });
