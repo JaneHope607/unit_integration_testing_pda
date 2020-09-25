5befd282377cf7e63c08e4151cd4049f6f22a30c
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-09-25 13:36:41 +0100

    arithermetial operations update display with result test passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -23,4 +23,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('44')
   })
 
+  it('arithmetical operations update the display with the result of the operation', function() {
+    element(by.css('#number4')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    running_total = element(by.css('#running_total'));
+    expect(running_total.getAttribute('value')).to.eventually.equal('0')
+  })
+
 });
